08215a8971070c0cb7d76f123010d99cef353a90
AnneWoepse <awoepse@gmail.com>
2014-10-15 16:30:40 -0400

    Commented DisplayEditActivity.java, DisplayTrialActivity.java, EditActivity.java and EditDefaultActivity.java

--- src/edu/upenn/cis350/cancerDog/DisplayTrialActivity.java
+++ src/edu/upenn/cis350/cancerDog/DisplayTrialActivity.java
@@ -7,6 +7,15 @@ import android.widget.ListView;
 
 public class DisplayTrialActivity extends ListActivity {
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle) Called when the
+	 * activity is first created. This is where you should do all of your normal
+	 * static set up: create views, bind data to lists, etc. This method also
+	 * provides you with a Bundle containing the activity's previously frozen
+	 * state, if there was one.
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Bundle extras = getIntent().getExtras();
